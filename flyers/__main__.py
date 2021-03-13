00ce3506b54e5e7532b7c275810bde3f7c85144a
mollinaca <dev@m.rel0.pw>
2021-03-13 16:09:50 +0900

    add

--- flyers/__main__.py
+++ flyers/__main__.py
@@ -3,6 +3,7 @@ import datetime, time
 import pathlib
 import configparser
 from . import f
+from .yorkmart import yorkmart
 
 def main ():
     dt_now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
@@ -14,11 +15,35 @@ def main ():
     webhook_dev = config['flyers']['webhook_dev']
     channel = config['flyers']['channel']
     channel_dev = config['flyers']['channel_dev']
+    updated = False
 
-    f.testf ()
-    message = "testdayo"
-    f.iw(webhook_dev,message)
+    stores = ['yorkmart']
+    pf = f.prev_flyer ()
 
+    for store in stores:
+        if store == 'yorkmart': # ヨークマート
+            york_flyers = yorkmart.get_flayers ()
+            if store not in pf['detail'] or not (set(york_flyers['flyers']) == set(pf['detail'][store]['flyers'])):
+                # pf に存在しないスーパー or pf にあるデータと一致しないURLを取得した → チラシに更新があった
+                # 新しいチラシを取得してPOSTする
+                updated = True
+                for flyer_url in york_flyers['flyers']:
+                    filename = f.dl (flyer_url)
+                    if not filename == 'Fail':
+                        f.files_upload (token, channel_dev, filename, flyer_url)
+                        os.remove (filename)
+                    else:
+                        message = 'チラシファイルのダウンロードに失敗しました。URL: ' + flyer_url
+                        f.iw (webhook_dev, message)
+            else:
+                text = '[debug] ' + store + ' has no changed.'
+                print (text)
+
+        else:
+            pass
+
+    if updated: # チラシに更新があった
+        pass
 
 if __name__ == '__main__':
     main ()
---
+++ flyers/yorkmart/yorkmart.py
@@ -0,0 +1,29 @@
+import datetime
+import requests
+from bs4 import BeautifulSoup
+
+def yorktest ():
+    print ("yorkmart!")
+    return 0
+
+def get_flayers () -> dict:
+    """
+    ヨークマートのチラシ情報
+    """
+    dt_now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+
+    # 最新のチラシのファイル名を取得
+    york_url = 'https://www.york-inc.com'
+    load_url = york_url + '/store/%e5%a4%a7%e5%ae%ae%e5%8d%97%e4%b8%ad%e9%87%8e%e5%ba%97.html'
+    res = requests.get (load_url)
+    html = BeautifulSoup (res.content, 'html.parser')
+    leaflet = html.find_all (class_='leaflet pc')
+    yorkmart_flyers = []
+    for l in leaflet:
+        for l2 in list (l.findAll('img')[0].get('data-set').split(',')):
+            if 'ul-files' in l2:
+                yorkmart_flyers.append (york_url + l2)
+
+    ret = {'updated_at':dt_now, 'flyers':yorkmart_flyers}
+    return ret
+
